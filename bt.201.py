a83d1f3b7ef3938f62ff1011f7fa51744f8c3337
Suxi <tranhoangnhu99@gmail.com>
2024-03-28 00:02:06 +0700

    Add code to draw a flower with 3 petals using turtle graphics

---
+++ bt.201.py
@@ -0,0 +1,28 @@
+import turtle
+
+# create flower with 3 petals using turtle graphics
+
+def draw_petal():
+    turtle.width(5)  # Set line thickness to 5
+    for _ in range(2):
+        turtle.forward(100)
+        turtle.right(60)
+        turtle.forward(100)
+        turtle.right(120)
+
+def draw_flower():
+    turtle.color('pink')
+    turtle.bgcolor('black')
+    turtle.goto(0, 0)
+    turtle.pendown()
+    for _ in range(3):
+        draw_petal()
+        turtle.right(120)
+    turtle.done()
+draw_flower()
+    
+
+
+
+
+
