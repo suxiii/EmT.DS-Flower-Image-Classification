dc7eff6219044e4d3d6b80f2efbc220b71b549c1
Suxi <tranhoangnhu99@gmail.com>
2024-04-17 23:18:59 +0700

    Refactor code to analyze sales data and optimize performance

---
+++ bt05.01.py
@@ -0,0 +1,56 @@
+import  pandas as pd
+
+# Đọc dữ liệu từ file sales_data.csv
+data = pd.read_csv('sales_data.csv', header=0, sep=",")
+data['Total'] = data['Quantity'] * data['Unit Price']
+data['Month'] = pd.to_datetime(data['Date']).dt.month
+data['Quarter'] = pd.to_datetime(data['Date']).dt.quarter
+
+# Tìm NV có kết quả kinh doanh cao nhất theo từng tháng
+result = data.groupby(['Month', 'Salesperson']).sum()
+result = result.reset_index()
+result = result.sort_values(by=['Month', 'Total'], ascending=False)
+result = result.drop_duplicates(subset='Month', keep='first')
+print(result[['Month', 'Salesperson', 'Total']])
+print('--------------------------------------')
+
+#Khu vực nào bán tốt trong quý 2 ?
+result = data.groupby(['Region', 'Quarter']).sum()
+result = result.reset_index()
+result = result[result['Quarter'] == 2]
+result = result.sort_values(by='Total', ascending=False)
+print(result[['Region', 'Total']])
+print('--------------------------------------')
+
+#Kết quả kinh doanh trung bình Sale theo khu vực
+result = data.groupby('Region').mean()
+result = result.reset_index()
+print(result[['Region', 'Total']])
+print('--------------------------------------')
+
+data = pd.read_csv('sales_data.csv')
+data['Total'] = data['Quantity'] * data['Unit Price']
+data['Month'] = pd.to_datetime(data['Date']).dt.month
+data['Quarter'] = pd.to_datetime(data['Date']).dt.quarter
+
+#Tìm NV có kết quả kinh doanh cao nhất theo từng tháng
+result = data.groupby(['Month', 'Salesperson']).sum()
+result = result.reset_index()
+result = result.sort_values(by=['Month', 'Total'], ascending=False)
+result = result.drop_duplicates(subset='Month', keep='first')
+print(result[['Month', 'Salesperson', 'Total']])
+print('--------------------------------------')
+
+#Khu vực nào bán tốt trong quý 2 ?
+result = data.groupby(['Region', 'Quarter']).sum()
+result = result.reset_index()
+result = result[result['Quarter'] == 2]
+result = result.sort_values(by='Total', ascending=False)
+print(result[['Region', 'Total']])
+print('--------------------------------------')
+
+#Kết quả kinh doanh trung bình Sale theo khu vực
+result = data.groupby('Region').mean()
+result = result.reset_index()
+print(result[['Region', 'Total']])
+print('--------------------------------------')
