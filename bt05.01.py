9747be3609f49e2cd2ab80a4934897753dd1704b
Suxi <tranhoangnhu99@gmail.com>
2024-04-19 23:46:51 +0700

    Refactor code to analyze sales data and optimize performance

--- bt05.01.py
+++ bt05.01.py
@@ -1,19 +1,6 @@
 import  pandas as pd
 
 # Đọc dữ liệu từ file sales_data.csv
-data = pd.read_csv('sales_data.csv', header=0, sep=",")
-data['Total'] = data['Quantity'] * data['Unit Price']
-data['Month'] = pd.to_datetime(data['Date']).dt.month
-data['Quarter'] = pd.to_datetime(data['Date']).dt.quarter
-
-# Tìm NV có kết quả kinh doanh cao nhất theo từng tháng
-result = data.groupby(['Month', 'Salesperson']).sum()
-result = result.reset_index()
-result = result.sort_values(by=['Month', 'Total'], ascending=False)
-result = result.drop_duplicates(subset='Month', keep='first')
-print(result[['Month', 'Salesperson', 'Total']])
-print('--------------------------------------')
-
 #Khu vực nào bán tốt trong quý 2 ?
 result = data.groupby(['Region', 'Quarter']).sum()
 result = result.reset_index()
